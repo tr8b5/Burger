a93dead115b583af321b29eff67b9c27cde03dc5
William Miller <williamhmiller12@gmail.com>
2021-02-26 22:10:37 -0500

    Deploying app

--- config/connection.js
+++ config/connection.js
@@ -1,12 +1,16 @@
 var mysql = require("mysql");
 
-var connection = mysql.createConnection({
+if (process.env.JAWSDB_URL) {
+  connection = mysql.createConnection(process.env.JAWSDB_URL);
+} else {
+  connection = mysql.createConnection({
   host: "localhost",
   port: 3306,
   user: "root",
   password: "William12",
   database: "burgers_db"
-});
+  })
+};
 
 
 connection.connect(function(err) {
